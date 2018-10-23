110698c192a88f56f05bd91a63aa9ab4a62384c9
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2018-10-23 16:22:52 +0300

    or maybe now

--- packages/dashboard/src/store/store.ts
+++ packages/dashboard/src/store/store.ts
@@ -1,14 +1,11 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import { userReducer } from './user/reducer'
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk"
+import { userReducer } from "./user/reducer"
 
-
-const reducer = (combineReducers({ user: userReducer }))
+const reducer = combineReducers({ user: userReducer })
 
 export const store = createStore(
-    reducer,
-    composeWithDevTools(
-        applyMiddleware(thunk)
-    )
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk)),
 )
--- packages/dashboard/src/store/user/reducer.ts
+++ packages/dashboard/src/store/user/reducer.ts
@@ -1,17 +1,17 @@
-import { ActionType, getType } from 'typesafe-actions'
-import { ITMCProfile } from '../../../../common/src/types'
-import * as user from './actions'
+import { ActionType, getType } from "typesafe-actions"
+import { ITMCProfile } from "../../../../common/src/types"
+import * as user from "./actions"
 
 type userState = ITMCProfile | null
 export type UserAction = ActionType<typeof user>
 
 export const userReducer = (state: userState = null, action: UserAction) => {
-    switch (action.type) {
-        case getType(user.add):
-            return action.payload
-        case getType(user.remove):
-            return null
-        default:
-            return state
-    }        
+  switch (action.type) {
+    case getType(user.add):
+      return action.payload
+    case getType(user.remove):
+      return null
+    default:
+      return state
+  }
 }
