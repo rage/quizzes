60a8bf4d8792ed0edd0638004e6e6fe8eda60e27
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2019-02-07 16:15:40 +0200

    endpoint for giving peer reviews

--- packages/backend/src/controllers/peerreviews/index.ts
+++ packages/backend/src/controllers/peerreviews/index.ts
@@ -1,4 +1,9 @@
-import { QuizAnswer } from "@quizzes/common/models"
+import {
+  PeerReview,
+  Quiz,
+  QuizAnswer,
+  UserQuizState,
+} from "@quizzes/common/models"
 import {
   BadRequestError,
   Body,
@@ -10,13 +15,16 @@ import {
   QueryParam,
   QueryParams,
 } from "routing-controllers"
-import { PeerReviewService } from "services/peerreview.service"
+import PeerReviewService from "services/peerreview.service"
+import QuizService from "services/quiz.service"
+import QuizAnswerService from "services/quizanswer.service"
+import UserQuizStateService from "services/userquizstate.service"
 import { Inject } from "typedi"
 import { EntityManager } from "typeorm"
 import { EntityFromBody } from "typeorm-routing-controllers-extensions"
 import { InjectManager } from "typeorm-typedi-extensions"
 
-@JsonController("/quizzes/:id/peerreview/:answerer")
+@JsonController("/quizzes/peerreview")
 export class PeerReviewController {
   @InjectManager()
   private entityManager: EntityManager
@@ -24,12 +32,84 @@ export class PeerReviewController {
   @Inject()
   private peerReviewService: PeerReviewService
 
+  @Inject()
+  private quizAnswerService: QuizAnswerService
+
+  @Inject()
+  private userQuizStateService: UserQuizStateService
+
+  @Inject()
+  private QuizService: QuizService
+
   @Get("/")
   public async get(
     @Param("id") id: string,
     @Param("answerer") answerer: string,
   ) {
     return await this.peerReviewService.getAnswersToReview(id, answerer)
+  }
+
+  @Post("/")
+  public async post(@EntityFromBody() peerReview: PeerReview): Promise<any> {
+    const receivingQuizAnswer: QuizAnswer = await this.quizAnswerService.getAnswer(
+      { id: peerReview.quizAnswerId },
+    )
+    const givingQuizAnswer: QuizAnswer = await this.quizAnswerService.getAnswer(
+      {
+        userId: peerReview.userId,
+        quizId: receivingQuizAnswer.quizId,
+        status: "submitted",
+      },
+    )
+
+    const receivingUserQuizState: UserQuizState = await this.userQuizStateService.getUserQuizState(
+      receivingQuizAnswer.userId,
+      receivingQuizAnswer.quizId,
+    )
+    const givingUserQuizState: UserQuizState = await this.userQuizStateService.getUserQuizState(
+      peerReview.userId,
+      receivingQuizAnswer.quizId,
+    )
+
+    const quiz = await this.QuizService.getQuizzes({
+      id: receivingQuizAnswer.quizId,
+    })
+
+    receivingUserQuizState.peerReviewsReceived += 1
+    givingUserQuizState.peerReviewsGiven += 1
+
+    await this.entityManager.transaction(async manager => {
+      await this.peerReviewService.createPeerReview(manager, peerReview)
+      const reveivingValidated = await this.peerReviewService.validateEssayAnswer(
+        manager,
+        quiz[0],
+        receivingQuizAnswer,
+        receivingUserQuizState,
+      )
+      const givingValidated = await this.peerReviewService.validateEssayAnswer(
+        manager,
+        quiz[0],
+        givingQuizAnswer,
+        givingUserQuizState,
+      )
+      await this.quizAnswerService.createQuizAnswer(
+        manager,
+        reveivingValidated.quizAnswer,
+      )
+      await this.quizAnswerService.createQuizAnswer(
+        manager,
+        givingValidated.quizAnswer,
+      )
+      await this.userQuizStateService.createUserQuizState(
+        manager,
+        reveivingValidated.userQuizState,
+      )
+      await this.userQuizStateService.createUserQuizState(
+        manager,
+        givingValidated.userQuizState,
+      )
+    })
+    return ""
   }
 
   /*@Post("/")
