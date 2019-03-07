b5ba395b476e8883cd393686a3b74f16cf54b75a
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2019-03-07 14:55:13 +0200

    fixed docker setup

--- ormconfig.js
+++ ormconfig.js
@@ -3,11 +3,12 @@ module.exports = {
     host: process.env.DB_HOST || "/var/run/postgresql",
     logging: !!process.env.DB_LOGGING || true,
     password: process.env.DB_PASSWORD || undefined,
-    synchronize: true,
+    synchronize: false,
     type: "postgres",
     username: process.env.DB_USER || undefined,
     migrations: ["dist/common/src/migration/*.js"],
     cli: {
         migrationsDir: "packages/common/src/migration"
-    }
+    },
+    entities: ["./dist/common/src/models/**/*.js"]
 }
