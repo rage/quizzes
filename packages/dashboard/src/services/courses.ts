ab0f39a7cb8ad6ce3aa7b729126aaa9f4c86fb1e
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2019-03-07 18:59:13 +0200

    dashboard docker config

--- packages/dashboard/src/services/courses.ts
+++ packages/dashboard/src/services/courses.ts
@@ -2,8 +2,11 @@ import axios from "axios"
 import TMCApi from "../../../common/src/services/TMCApi"
 
 export const getCourses = async user => {
-  const response = await axios.get("http://localhost:3000/api/v1/courses", {
-    headers: { authorization: `Bearer ${user.accessToken}` },
-  })
+  const response = await axios.get(
+    `${process.env.REACT_APP_BACKEND_URL}/api/v1/courses`,
+    {
+      headers: { authorization: `Bearer ${user.accessToken}` },
+    },
+  )
   return response.data
 }
--- packages/dashboard/src/services/quizzes.ts
+++ packages/dashboard/src/services/quizzes.ts
@@ -2,7 +2,9 @@ import axios from "axios"
 
 export const getQuizzes = async (course, user) => {
   const response = await axios.get(
-    `http://localhost:3000/api/v1/quizzes/?courseId=${course}&course=true&items=true&options=true&peerreviews=true&stripped=false`,
+    `${
+      process.env.REACT_APP_BACKEND_URL
+    }/api/v1/quizzes/?courseId=${course}&course=true&items=true&options=true&peerreviews=true&stripped=false`,
     { headers: { authorization: `Bearer ${user.accessToken}` } },
   )
   return response.data
@@ -10,7 +12,7 @@ export const getQuizzes = async (course, user) => {
 
 export const post = async (quiz, user) => {
   const response = await axios.post(
-    "http://localhost:3000/api/v1/quizzes",
+    `${process.env.REACT_APP_BACKEND_URL}/api/v1/quizzes`,
     quiz,
     {
       headers: {
