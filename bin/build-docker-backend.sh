1c37b709afe8dc9036cd0c18fba50c5279fba627
Henrik Nygren <nygrenh@gmail.com>
2019-03-07 23:27:24 +0200

    New build

---
+++ bin/build-docker-backend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+REV=$(git rev-parse --verify HEAD)
+DATE=$(date +%s)
+TAG="gcr.io/moocfi/quizzes-backend:build-$DATE-$REV"
+echo Building $TAG
+docker build . -f Dockerfile.backend -t "$TAG"
