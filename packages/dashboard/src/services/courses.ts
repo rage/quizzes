36128ce2fb72dd72f99f343c930b331f3bf40280
Henrik Nygren <nygrenh@gmail.com>
2019-03-08 15:47:29 +0200

    Update production configs

--- packages/dashboard/src/services/courses.ts
+++ packages/dashboard/src/services/courses.ts
@@ -2,11 +2,8 @@ import axios from "axios"
 import TMCApi from "../../../common/src/services/TMCApi"
 
 export const getCourses = async user => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_BACKEND_URL}/api/v1/courses`,
-    {
-      headers: { authorization: `Bearer ${user.accessToken}` },
-    },
-  )
+  const response = await axios.get(`/api/v1/courses`, {
+    headers: { authorization: `Bearer ${user.accessToken}` },
+  })
   return response.data
 }
--- packages/dashboard/src/services/quizzes.ts
+++ packages/dashboard/src/services/quizzes.ts
@@ -2,24 +2,18 @@ import axios from "axios"
 
 export const getQuizzes = async (course, user) => {
   const response = await axios.get(
-    `${
-      process.env.REACT_APP_BACKEND_URL
-    }/api/v1/quizzes/?courseId=${course}&course=true&items=true&options=true&peerreviews=true&stripped=false`,
+    `/api/v1/quizzes/?courseId=${course}&course=true&items=true&options=true&peerreviews=true&stripped=false`,
     { headers: { authorization: `Bearer ${user.accessToken}` } },
   )
   return response.data
 }
 
 export const post = async (quiz, user) => {
-  const response = await axios.post(
-    `${process.env.REACT_APP_BACKEND_URL}/api/v1/quizzes`,
-    quiz,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${user.accessToken}`,
-      },
+  const response = await axios.post(`/api/v1/quizzes`, quiz, {
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${user.accessToken}`,
     },
-  )
+  })
   return response.data
 }
