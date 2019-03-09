bfe97717517b48dee14fcd25d843d3fa3d899783
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-03-09 11:35:57 +0200

    Basic structure in place and the success of the answer indicated by color

--- packages/widgets/src/components/Open.jsx
+++ packages/widgets/src/components/Open.jsx
@@ -1,4 +1,76 @@
 import React from 'react'
+import { TextField, Typography, Paper } from '@material-ui/core'
 
 
-export default () => <div>An open field</div>
+
+const fieldInstruction = (languageId) => {
+    const instructionOptions = {
+        en_US: "Answer...",
+        fi_FI: "Vastaus..."
+    }
+    return instructionOptions[languageId] !== undefined ? 
+            instructionOptions[languageId]
+            : "..."
+}
+
+
+const Open = ({
+    answered,
+    correct,
+    handleTextDataChange,
+    languageId,
+    textData,
+    successMessage,
+    failureMessage
+}) => {
+
+
+
+    if(answered){
+        return (
+            <div>
+                <Typography variant="subtitle1">Vastasit</Typography>
+                <Paper style={paper}>
+                    <Typography variant="body1">{ textData }</Typography>
+                </Paper>
+                <Paper style={answerStyle(correct)} >
+                    <Typography variant="body1">
+                    { correct ? 
+                    successMessage : 
+                    failureMessage }
+                    </Typography>
+                </Paper>
+            </div>
+        )
+    }
+    
+
+
+    return (
+        <div>
+            <TextField 
+            value={textData}
+            onChange={handleTextDataChange}
+            fullWidth
+            margin="normal"
+            placeholder={fieldInstruction(languageId)}
+            />
+        </div>
+    )
+}
+
+
+const answerStyle = (correct) => ({  
+    ...paper,
+    borderLeft: `1em solid ${correct ? "green" : "red"}`
+    
+})
+
+
+const paper = {
+    padding: 10,
+    margin: 10
+}
+
+
+export default Open
