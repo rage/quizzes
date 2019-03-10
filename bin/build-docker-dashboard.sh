6c1f5e4f31dd04fb502e1efeea224e1fea583fce
Henrik Nygren <nygrenh@gmail.com>
2019-03-10 20:22:06 +0200

    Add a script for building dashboard

--- bin/build-docker-dashboard.sh
+++ bin/build-docker-dashboard.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 REV=$(git rev-parse --verify HEAD)
 DATE=$(date +%s)
-TAG="gcr.io/moocfi/quizzes-backend:build-$DATE-$REV"
+TAG="gcr.io/moocfi/quizzes-dashboard:build-$DATE-$REV"
 echo Building "$TAG"
-docker build . -f Dockerfile.backend -t "$TAG"
+docker build . -f Dockerfile.dashboard -t "$TAG"
