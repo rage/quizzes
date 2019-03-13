ba41b26c6e7cab6efea0e22b2ef075db6a2b5729
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-03-13 13:11:41 +0200

    Boxes disabled if answered, initial connection between answer state and widget
    If multiple checkboxes, the optionAnswers don't yet reflect the true state of the form. With only one checkbox this should work correctly.
    Radio button support still unsupported.

--- packages/widgets/src/components/ResearchAgreement.jsx
+++ packages/widgets/src/components/ResearchAgreement.jsx
@@ -1,21 +1,76 @@
 import React from "react"
 import { Checkbox, Grid } from "@material-ui/core"
 
-const ResearchAgreement = ({ options, item }) => {
-  console.log(options)
+const Option = ({ label, value, toggle, answered }) => {
+  let checkboxOptions = {
+    disabled: answered,
+  }
+  if (answered) {
+    checkboxOptions.checked = true
+  }
+
+  return (
+    <Grid container style={{ marginBottom: "center" }}>
+      <Grid item xs={1}>
+        <Checkbox
+          value={value}
+          color="primary"
+          onChange={toggle}
+          {...checkboxOptions}
+        />
+      </Grid>
+      <Grid item xs style={{ alignSelf: "center" }}>
+        {label}
+      </Grid>
+    </Grid>
+  )
+}
+
+const ResearchAgreement = ({
+  options,
+  optionAnswers,
+  item,
+  answered,
+  handleOptionChange,
+}) => {
+  console.log("Option answers: ", optionAnswers)
+
+  const handleUnchecking = handleOptionChange(-1)
+
+  const toggle = optionId => {
+    const oa = optionAnswers.find(oa => oa.quizOptionId === optionId)
+    const handleChecking = handleOptionChange(optionId)
+    return () => {
+      if (!oa) {
+        handleChecking()
+      } else {
+        handleUnchecking()
+      }
+    }
+  }
+  ;() => {
+    if (!optionAnswers[0]) {
+      handleChecking()
+    } else {
+      handleUnchecking()
+    }
+  }
 
   return (
     <React.Fragment>
       {options.map(option => {
+        const currentAnswer = optionAnswers.find(
+          oa => oa.quizOptionId === option.id,
+        )
+
         return (
-          <Grid container style={{ marginBottom: 10 }} key={option.id}>
-            <Grid item xs={1}>
-              <Checkbox color="primary" />
-            </Grid>
-            <Grid item xs style={{ alignSelf: "center" }}>
-              {option.texts[0].title}
-            </Grid>
-          </Grid>
+          <Option
+            key={option.id}
+            label={option.texts[0].title}
+            value={currentAnswer ? currentAnswer.quizOptionId : ""}
+            toggle={toggle(option.id)}
+            answered={answered}
+          />
         )
       })}
     </React.Fragment>
