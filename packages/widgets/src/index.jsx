489c984edbf3704633d3f8660abf1ab4a8bf69d6
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2019-03-18 19:17:41 +0200

    fixd

--- packages/widgets/src/index.jsx
+++ packages/widgets/src/index.jsx
@@ -6,11 +6,7 @@ const root = document.getElementById("root")
 
 render(
   <div style={{ width: "100%", maxWidth: 800, margin: "0 auto" }}>
-    <Quiz
-      id="a28fc803-8173-4df9-bf73-d6e40e96ce0f"
-      languageId="fi_FI"
-      accessToken="1436f0ed8869efc9d89ce0b6706d9ba07747490e2ed5b2ef3dd18caf0f0ac04a"
-    />
+    <Quiz id="" languageId="fi_FI" accessToken="" />
   </div>,
   root,
 )
