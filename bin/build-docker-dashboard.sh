8269fd6f87150e12e3f6004304d08c4cbf2dbd31
Henrik Nygren <nygrenh@gmail.com>
2019-03-18 07:45:11 +0200

    Setup circle

--- bin/build-docker-dashboard.sh
+++ bin/build-docker-dashboard.sh
@@ -6,3 +6,6 @@ DATE=$(date +%s)
 TAG="gcr.io/moocfi/quizzes-dashboard:build-$DATE-$REV"
 echo Building "$TAG"
 docker build . -f Dockerfile.dashboard -t "$TAG"
+
+echo "export QUIZZES_DASHBOARD_IMAGE=$TAG"
+export QUIZZES_DASHBOARD_IMAGE=$TAG
---
+++ bin/ci-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+BIN_DIR=$(dirname "$0")
+
+echo "Building dashboard..."
+echo source "$BIN_DIR/build-docker-dashboard.sh"
+
+echo "Building backend..."
+echo source "$BIN_DIR/build-docker-backend.sh"
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "master" ]]; then
+  echo "We're not in master. Stopping here.";
+  exit 0;
+fi
+
+echo "We are on master. We probably should deploy.";
