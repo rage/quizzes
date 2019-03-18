d1031f2ad1b9cf11a4247ead9b27daceaab0c87e
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2019-03-18 19:15:32 +0200

    small fix: alter -> update

--- packages/backend/src/migration/1552928936202-alter_tilpe_quiz_answer.ts
+++ packages/backend/src/migration/1552928936202-alter_tilpe_quiz_answer.ts
@@ -4,13 +4,13 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class alterTilpeQuizAnswer1552928936202 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      "alter quiz_answer set status = 'confirmed' where quiz_id = 'a18fe51a-80a8-41a7-99a5-d591ccba2f9a'",
+      "update quiz_answer set status = 'confirmed' where quiz_id = 'a18fe51a-80a8-41a7-99a5-d591ccba2f9a'",
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      "alter quiz_answer set status = 'submitted' where quiz_id = 'a18fe51a-80a8-41a7-99a5-d591ccba2f9a'",
+      "update quiz_answer set status = 'submitted' where quiz_id = 'a18fe51a-80a8-41a7-99a5-d591ccba2f9a'",
     )
   }
 }
