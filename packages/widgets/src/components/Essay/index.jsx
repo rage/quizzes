663da51ba0669f8809e2e62595facb0e3c61e3f3
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-03-18 12:03:06 +0200

    Title and body can be added to essay quiz items
    They are also displayed in the widget. Submitting answers does not work yet.

--- packages/widgets/src/components/Essay/index.jsx
+++ packages/widgets/src/components/Essay/index.jsx
@@ -5,6 +5,13 @@ import StageVisualizer from "./StageVisualizer"
 export default props => {
   return (
     <div>
+      {(props.item.texts[0].title || props.item.texts[0].body) && (
+        <div>
+          <h2>{props.item.texts[0].title}</h2>
+          {props.item.texts[0].body}
+        </div>
+      )}
+
       <StageVisualizer {...props} />
       <StageSelector {...props} />
     </div>
