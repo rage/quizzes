4b5d044e8222aefec2ff0dff69f4dfad5b1b1682
Kuutti Jesper M <jekuutti@lx8-500-10302.pc.helsinki.fi>
2019-05-06 16:40:11 +0300

    Changed from button component to similarly looking but better draggable

--- packages/dashboard/src/components/MultipleChoice/SortableOptionList.tsx
+++ packages/dashboard/src/components/MultipleChoice/SortableOptionList.tsx
@@ -1,7 +1,8 @@
-import { Button, Grid, IconButton } from "@material-ui/core"
+import { Button, Grid, IconButton, Typography } from "@material-ui/core"
 import AddCircle from "@material-ui/icons/AddCircle"
 import React from "react"
 import { SortableContainer } from "react-sortable-hoc"
+import DragHandleWrapper from "../DragHandleWrapper"
 import SortableWrapper from "../SortableWrapper"
 
 const SortableOptionList = SortableContainer((props: any) => {
@@ -18,19 +19,21 @@ const SortableOptionList = SortableContainer((props: any) => {
           index={index}
           key={`${option.quizItemId}-${index}-${option.texts[0].title}`}
         >
-          <Grid item={true} xs="auto" style={{ textAlign: "center" }}>
-            <Button
-              variant="outlined"
-              style={{
-                borderColor: option.correct ? "green" : "red",
-                borderStyle: "dotted",
-                borderWidth: ".25em",
-                textTransform: "none",
-              }}
-              onClick={props.modifyExistingOption(option.id, option.quizItemId)}
-            >
-              {option.texts[0].title}
-            </Button>
+          <Grid item={true} xs="auto" style={{ 
+            textAlign: "center",
+            border: "dotted",
+            borderColor: option.correct ? "green" : "red",
+            cursor: "pointer"
+         }}
+         onClick={props.modifyExistingOption(option.id, option.quizItemId)}
+         >
+
+               <Typography 
+             
+               >
+                  { option.texts[0].title }
+               </Typography>
+ 
           </Grid>
         </SortableWrapper>
       ))}
