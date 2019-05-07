e177370ec3cc99f753d1b4727b7ed61145b2ba1b
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-05-07 09:52:02 +0300

    Word-breaks more aggressive if the option title contains a very long word

--- packages/dashboard/src/components/MultipleChoice/SortableOptionList.tsx
+++ packages/dashboard/src/components/MultipleChoice/SortableOptionList.tsx
@@ -2,7 +2,7 @@ import { Button, Grid, IconButton, Typography } from "@material-ui/core"
 import AddCircle from "@material-ui/icons/AddCircle"
 import React from "react"
 import { SortableContainer } from "react-sortable-hoc"
-import DragHandleWrapper from "../DragHandleWrapper"
+import { stringContainsLongerWord } from "../../../../common/src/util/index"
 import SortableWrapper from "../SortableWrapper"
 
 const SortableOptionList = SortableContainer((props: any) => {
@@ -11,32 +11,38 @@ const SortableOptionList = SortableContainer((props: any) => {
       container={true}
       spacing={16}
       justify="space-evenly"
-      alignItems="stretch"
+      alignItems="center"
     >
-      {props.options.map((option, index) => (
-        <SortableWrapper
-          collection={`items[${props.order}].options`}
-          index={index}
-          key={`${option.quizItemId}-${index}-${option.texts[0].title}`}
-        >
-          <Grid item={true} xs="auto" style={{ 
-            textAlign: "center",
-            border: "dotted",
-            borderColor: option.correct ? "green" : "red",
-            cursor: "pointer"
-         }}
-         onClick={props.modifyExistingOption(option.id, option.quizItemId)}
-         >
-
-               <Typography 
-             
-               >
-                  { option.texts[0].title }
-               </Typography>
- 
-          </Grid>
-        </SortableWrapper>
-      ))}
+      {props.options.map((option, index) => {
+        return (
+          <SortableWrapper
+            collection={`items[${props.order}].options`}
+            index={index}
+            key={`${option.quizItemId}-${index}-${option.texts[0].title}`}
+          >
+            <Grid item={true} xs={12} sm={6} md={4} lg={3}>
+              <div
+                style={{
+                  border: "dotted",
+                  borderColor: option.correct ? "green" : "red",
+                  cursor: "pointer",
+                  padding: ".5em",
+                  textAlign: "center",
+                  wordBreak: stringContainsLongerWord(option.texts[0].title, 30)
+                    ? "break-all"
+                    : "normal",
+                }}
+                onClick={props.modifyExistingOption(
+                  option.id,
+                  option.quizItemId,
+                )}
+              >
+                <Typography variant="body1">{option.texts[0].title}</Typography>
+              </div>
+            </Grid>
+          </SortableWrapper>
+        )
+      })}
       <Grid item={true} xs="auto">
         <IconButton
           aria-label="Add option"
