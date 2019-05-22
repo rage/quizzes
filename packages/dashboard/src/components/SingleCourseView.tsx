55dd5a83ea76da50dc9d6fcb429d44e61d40cc7d
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-05-22 18:56:22 +0300

    Initial listing of quizzes according to part/section

--- packages/dashboard/src/components/SingleCourseView.tsx
+++ packages/dashboard/src/components/SingleCourseView.tsx
@@ -1,5 +1,7 @@
 import {
   Button,
+  Grid,
+  IconButton,
   MenuItem,
   Select,
   Table,
@@ -10,6 +12,7 @@ import {
   Toolbar,
   Typography,
 } from "@material-ui/core"
+import Flag from "@material-ui/icons/Flag"
 import React from "react"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
@@ -90,6 +93,178 @@ class SingleCourseView extends React.Component<any, any> {
   }
 }
 
+class NewCourseView extends React.Component<any, any> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      parts: {},
+      initialized: false,
+    }
+  }
+
+  public componentDidUpdate() {
+    if (
+      !this.state.initialized &&
+      this.props.quizzes &&
+      this.props.quizzes.length > 0 &&
+      this.props.filter.course
+    ) {
+      const newParts = {}
+
+      this.props.quizzes
+        .filter(q => q.courseId === this.props.filter.course)
+        .forEach(q => {
+          let sections = newParts[`${q.part}`]
+          if (!sections) {
+            sections = {}
+            newParts[`${q.part}`] = sections
+          }
+          let section = sections[`${q.section}`]
+          if (!section) {
+            section = []
+          }
+          section = section.concat(q)
+          sections[`${q.section}`] = section
+        })
+
+      this.setState({
+        parts: newParts,
+        initialized: true,
+      })
+    }
+  }
+
+  public componentDidMount() {
+    this.setState({ initialized: false })
+
+    if (
+      this.props.match.params.id &&
+      (!this.props.filter.course ||
+        this.props.filter.course !== this.props.match.params.id)
+    ) {
+      this.props.setCourseTo(this.props.match.params.id)
+    }
+  }
+
+  public render() {
+    if (this.props.courses.length === 0) {
+      return <div />
+    }
+    const currentCourse = this.props.courses.find(
+      course => this.props.filter.course === course.id,
+    )
+    if (!currentCourse) {
+      return <div />
+    }
+
+    return (
+      <Grid container={true} justify="center" alignItems="center" spacing={16}>
+        <Grid item={true} xs={10}>
+          <Grid
+            container={true}
+            justify="center"
+            alignItems="stretch"
+            spacing={16}
+          >
+            <Grid item={true} xs="auto">
+              <Typography variant="title">
+                {currentCourse.texts[0] &&
+                  currentCourse.texts[0].title.toUpperCase()}
+              </Typography>
+            </Grid>
+
+            <LanguageBar />
+
+            <Grid item={true} xs="auto">
+              <Typography variant="subtitle1">
+                QUIZZES ON THIS COURSE
+              </Typography>
+            </Grid>
+            <Grid item={true} xs={12} style={{ backgroundColor: "#F8F8F8" }}>
+              <Grid container={true} justify="flex-start" alignItems="center">
+                {JSON.stringify(this.state.parts) !== "{}" &&
+                  Object.keys(this.state.parts).map(part => {
+                    return (
+                      <PartComponent
+                        key={part}
+                        partNumber={part}
+                        sections={this.state.parts[part]}
+                      />
+                    )
+                  })}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+const LanguageBar = props => {
+  return (
+    <Grid item={true} xs={12} style={{ backgroundColor: "#F8F8F8" }}>
+      <Grid container={true} justify="space-between">
+        <Grid item={true} xs="auto">
+          <IconButton>
+            <Flag />
+          </IconButton>
+          <IconButton>
+            <Flag />
+          </IconButton>
+          <IconButton>
+            <Flag />
+          </IconButton>
+        </Grid>
+        <Grid item={true} xs="auto">
+          <Button
+            variant="text"
+            style={{
+              borderRadius: "0px",
+              backgroundColor: "#107EAB",
+              color: "white",
+            }}
+          >
+            Add language
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+const PartComponent = ({ sections, partNumber }) => {
+  return (
+    <React.Fragment>
+      <Typography>Part {partNumber} </Typography>
+      {Object.keys(sections).map(section => {
+        return (
+          <SectionComponent
+            key={section}
+            sectionNumber={section}
+            quizzes={sections[section]}
+          />
+        )
+      })}
+    </React.Fragment>
+  )
+}
+
+const SectionComponent = ({ quizzes, sectionNumber }) => {
+  return (
+    <React.Fragment>
+      <Typography>Section {sectionNumber}</Typography>
+      {quizzes.map(q => {
+        return (
+          <Grid item={true} xs={12} key={q.id}>
+            <Button>{q.texts[0].title}</Button>
+          </Grid>
+        )
+      })}
+    </React.Fragment>
+  )
+}
+
 const mapStateToProps = (state: any) => {
   return {
     courses: state.courses,
@@ -105,4 +280,4 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(SingleCourseView)
+)(NewCourseView)
