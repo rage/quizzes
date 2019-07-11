2e1f0f8ee417d7031e8f618263ee2afb884b4417
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-07-11 15:20:36 +0300

    Attempt to test widgets w/ circle ci

---
+++ bin/build-docker-widgets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+
+DATE=$(date +%s)
+
+if [ -n "$CIRCLE_SHA1" ]; then
+  echo "Running in Circle CI"
+  REV="$CIRCLE_WORKFLOW_ID-$(git rev-parse --verify HEAD)"
+else
+  echo "Running outside CI"
+  REV="$DATE-$(git rev-parse --verify HEAD)"
+fi
+
+TAG="eu.gcr.io/moocfi/quizzes-widgets:build-$REV"
+echo Building "$TAG"
+docker build . -f Dockerfile.widgets -t "$TAG"
+
+echo "Successfully built image: $TAG"
