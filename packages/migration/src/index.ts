804a3493f208e6398e3ddd24764f9a9f3deb8b67
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2019-07-12 11:33:02 +0300

    save test

--- packages/migration/src/index.ts
+++ packages/migration/src/index.ts
@@ -41,14 +41,49 @@ const data = {
       spamFlags: 0,
       __v: 0,
     },
+    {
+      _id: "5af9761d54198f0356e5047a",
+      updatedAt: "2018-05-14T11:42:21.814Z",
+      createdAt: "2018-05-14T11:42:21.814Z",
+      data: "jh5zmqdh7",
+      answererId: "valuable-snowy-owl-82856",
+      quizId: "5af9485cdd01ab0004371af7",
+      confirmed: true,
+      peerReviewCount: 0,
+      spamFlags: 0,
+      __v: 0,
+    },
+    {
+      _id: "5af9986674bd070285bf7924",
+      updatedAt: "2018-05-14T14:08:38.217Z",
+      createdAt: "2018-05-14T14:08:38.217Z",
+      data: {
+        jh1ozgf6t: "jh1ou3jr1l",
+        jh1ouau4c: "jh1ou32l1f",
+        jh1ouaew1l: "jh1otzwjp",
+        jh1ou9uw1n: "jh1ou0qr1v",
+      },
+      answererId: "014707302",
+      quizId: "5af55153bc22a00004917f84",
+      confirmed: true,
+      peerReviewCount: 0,
+      spamFlags: 0,
+      __v: 0,
+    },
   ],
 }
 
-const user = new User()
-user.id = 34176
+const user1 = new User()
+user1.id = 34176
+const user2 = new User()
+user2.id = 33165
+const user3 = new User()
+user3.id = 14667
 
 const users = {
-  "sparkling-earwig-67645": user,
+  "sparkling-earwig-67645": user1,
+  "valuable-snowy-owl-82856": user2,
+  "014707302": user3,
 }
 
 if (process.env.NODE_ENV !== "production") {
@@ -61,19 +96,37 @@ async function main() {
   const connection = await database.connect()
   const manager = connection.createEntityManager()
 
-  const quiz: Quiz = await manager
+  const quiz: Quiz[] = await manager
     .createQueryBuilder(Quiz, "quiz")
     .leftJoinAndSelect("quiz.course", "course")
     .leftJoinAndSelect("course.texts", "course_translation")
     .leftJoinAndSelect("course.languages", "language")
     .leftJoinAndSelect("quiz.items", "item")
     .leftJoinAndSelect("item.options", "option")
-    .where("quiz.id = :id", { id: "3ec7c1cc-27f5-4518-890a-201a9fe6121d" })
-    .getOne()
+    .where("quiz.id in (:...ids)", {
+      ids: [
+        "3ec7c1cc-27f5-4518-890a-201a9fe6121d",
+        "3bb8bfa5-2602-4f37-928f-1e8a3794a83b",
+        "27dab5d3-fda9-4ff8-acc7-cbcdaa1598d6",
+      ],
+    })
+    .getMany()
+
+  console.log(quiz)
 
   const quizzes = {
-    "3ec7c1cc-27f5-4518-890a-201a9fe6121d": quiz,
+    "3ec7c1cc-27f5-4518-890a-201a9fe6121d": quiz.find(
+      q => q.id === "3ec7c1cc-27f5-4518-890a-201a9fe6121d",
+    ),
+    "3bb8bfa5-2602-4f37-928f-1e8a3794a83b": quiz.find(
+      q => q.id === "3bb8bfa5-2602-4f37-928f-1e8a3794a83b",
+    ),
+    "27dab5d3-fda9-4ff8-acc7-cbcdaa1598d6": quiz.find(
+      q => q.id === "27dab5d3-fda9-4ff8-acc7-cbcdaa1598d6",
+    ),
   }
+
+  console.log(quizzes)
 
   /*const migrations = await manager.query(
     "select date from migration order by date desc limit 1",
