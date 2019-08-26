878ab9f9583db78292391247e3a83b55d18920ff
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-08-26 22:24:53 +0300

    Created a new table for course roles

---
+++ packages/backend/src/models/user_course_role.ts
@@ -0,0 +1,40 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm"
+import { Course } from "./course"
+import { User } from "./user"
+
+type CourseRole = "teacher" | "assistant"
+
+@Entity()
+export class UserCourseRole extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  public id: string
+
+  @ManyToOne(type => User, user => user.id)
+  public user: Promise<User>
+  @Column({ nullable: false })
+  public userId: number
+
+  @ManyToOne(type => Course, course => course.id)
+  public course: Promise<Course>
+  @Column({ nullable: false })
+  public courseId: string
+
+  @Column({
+    type: "varchar",
+    nullable: false,
+  })
+  public role: CourseRole
+
+  @CreateDateColumn({ type: "timestamp" })
+  public createdAt: Date
+  @UpdateDateColumn({ type: "timestamp" })
+  public updatedAt: Date
+}
