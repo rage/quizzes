5b0378808bcc352e13642b440609ba28bebdac75
Henrik Nygren <nygrenh@gmail.com>
2019-10-01 18:17:45 +0300

    Get db user and password from env in kafka publish

--- packages/backend/src/scripts/kafka_batch_publisher.ts
+++ packages/backend/src/scripts/kafka_batch_publisher.ts
@@ -21,6 +21,8 @@ const knex = Knex({
   connection: {
     host: process.env.DB_HOST || "/var/run/postgresql",
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
 })
 
