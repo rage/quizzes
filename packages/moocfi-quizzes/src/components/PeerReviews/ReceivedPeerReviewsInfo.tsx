60f6a2a8cca0e6d6f453fb47ad0926676a992cbd
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-11-26 11:28:04 +0200

    Prepared to show review data in widget

--- packages/moocfi-quizzes/src/components/PeerReviews/ReceivedPeerReviewsInfo.tsx
+++ packages/moocfi-quizzes/src/components/PeerReviews/ReceivedPeerReviewsInfo.tsx
@@ -1,20 +1,93 @@
 import * as React from "react"
-import { Button } from "@material-ui/core"
+import { Button, Typography } from "@material-ui/core"
+import {
+  PeerReviewAnswer,
+  PeerReviewQuestionAnswer,
+  PeerReviewGradeAnswer,
+  PeerReviewEssayAnswer,
+} from "../../modelTypes"
 
 const ReceivedPeerReviews: React.FunctionComponent<any> = () => {
   const [modalOpened, setModalOpened] = React.useState(false)
 
+  const receivedReviews: PeerReviewAnswer[] = [
+    {
+      quizAnswerId: "weui",
+      peerReviewCollectionId: "safd",
+      userId: 666,
+      rejectedQuizAnswerIds: [],
+      answers: [
+        {
+          peerReviewQuestionId: "111-222",
+          value: 3,
+        },
+        {
+          peerReviewQuestionId: "222-222",
+          text: "Oikein huono",
+        },
+      ],
+    },
+  ]
+
   return (
     <>
       {modalOpened ? (
-        <div>Now showing the detailed info of all the reviews</div>
+        <ReceivedReviewsDetailed peerReviews={receivedReviews} />
       ) : (
-        <div>Showing the summary of the peer reviews you've received</div>
+        <ReceivedReviewsSummary peerReviews={receivedReviews} />
       )}
       <Button variant="outlined" onClick={() => setModalOpened(!modalOpened)}>
         Toggle mode
       </Button>
     </>
+  )
+}
+
+interface IReceivedReviewsProps {
+  peerReviews?: PeerReviewAnswer[]
+}
+
+const ReceivedReviewsDetailed: React.FunctionComponent<
+  IReceivedReviewsProps
+> = ({ peerReviews }) => {
+  if (!peerReviews) {
+    return <div>Info not available (may be loading)</div>
+  }
+
+  return (
+    <div>
+      <h2>Details of all the reviews</h2>
+      {peerReviews
+        .map(r =>
+          r.answers
+            .map(ans => {
+              let gradingPart
+              if ((ans as PeerReviewGradeAnswer).value) {
+                gradingPart = (ans as PeerReviewGradeAnswer).value
+              } else {
+                gradingPart = (ans as PeerReviewEssayAnswer).text
+              }
+              return ans.peerReviewQuestionId + ": " + gradingPart
+            })
+            .join(" , "),
+        )
+        .join(" ; ")}
+    </div>
+  )
+}
+
+const ReceivedReviewsSummary: React.FunctionComponent<
+  IReceivedReviewsProps
+> = ({ peerReviews }) => {
+  if (!peerReviews) {
+    return <div>Info not available (may be loading)</div>
+  }
+
+  return (
+    <div>
+      <h2>Overview of the reviews</h2>
+      Number of the reviews you've received: {peerReviews.length}
+    </div>
   )
 }
 
