adca4fba19911573b0778692314b286668d6c900
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-12-17 17:16:52 +0200

    Revert to not using regex in peer reviews controller

--- packages/backend/src/controllers/peerreviews/index.ts
+++ packages/backend/src/controllers/peerreviews/index.ts
@@ -139,7 +139,7 @@ export class PeerReviewController {
     return stringStream
   }
 
-  @Get("/:quizId/:languageId(([a-z]|[A-Z]|_){5,8})")
+  @Get("/:quizId/:languageId")
   public async get(
     @Param("quizId") quizId: string,
     @Param("languageId") languageId: string,
