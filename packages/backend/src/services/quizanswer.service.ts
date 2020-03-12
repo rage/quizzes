c491c116c0a9e456361b9dcf682314d610115d81
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2020-03-12 22:39:38 +0200

    Course roles affect which courses are considered in counts

--- packages/backend/src/services/quizanswer.service.ts
+++ packages/backend/src/services/quizanswer.service.ts
@@ -172,6 +172,8 @@ export default class QuizAnswerService {
   public async getAnswersCount(query: IQuizAnswerQuery): Promise<any> {
     let result
 
+    const permittedCourseIds = query.courseIds
+
     // Temporarily check elements courses counts differently from others
     // Later: save the attention criteria in database for each course
     // (and maybe don't request the numbers for all the courses in one request)
@@ -181,26 +183,47 @@ export default class QuizAnswerService {
       // Assuming that all the relevant quizzes on all elements courses use the same values, which are hard-coded below
       // TODO: change query building to support option "course-minimum" for the relevant properties
 
-      const elementsCourseIds: string[] = await this.courseService.getElementsCourseIds()
-      const elementsCriteria: IQuizAnswerQuery = {
-        courseIds: elementsCourseIds,
-        courseIdIncludedInCourseIds: true,
-        statuses: ["spam", "submitted", "enough-received-but-not-given"],
-        minPeerReviewsGiven: 3,
-        minPeerReviewsReceived: 2,
-        minSpamFlagsOr: 1,
+      let elementsCourseIds: string[] = await this.courseService.getElementsCourseIds()
+
+      // undefined for admins
+      if (permittedCourseIds) {
+        elementsCourseIds = elementsCourseIds.filter(id =>
+          permittedCourseIds.includes(id),
+        )
       }
-      elementsCriteria.courseIdIncludedInCourseIds = true
+
+      let elementsCriteria: IQuizAnswerQuery
+      if (elementsCourseIds.length > 0) {
+        elementsCriteria = {
+          courseIds: elementsCourseIds,
+          courseIdIncludedInCourseIds: true,
+          statuses: ["spam", "submitted", "enough-received-but-not-given"],
+          minPeerReviewsGiven: 3,
+          minPeerReviewsReceived: 2,
+          minSpamFlagsOr: 1,
+        }
+        elementsCriteria.courseIdIncludedInCourseIds = true
+      }
 
       const nonElementsCriteria = { ...query }
 
-      nonElementsCriteria.courseIds = elementsCourseIds
-      nonElementsCriteria.courseIdIncludedInCourseIds = false
+      if (permittedCourseIds) {
+        nonElementsCriteria.courseIds = permittedCourseIds.filter(
+          id => !elementsCourseIds.includes(id),
+        )
+      } else {
+        nonElementsCriteria.courseIds = elementsCourseIds
+        nonElementsCriteria.courseIdIncludedInCourseIds = false
+      }
 
-      const [result1, result2] = await Promise.all([
-        this.getQuizCounts(elementsCriteria),
-        this.getQuizCounts(nonElementsCriteria),
-      ])
+      const promise1: Promise<any> | [] =
+        elementsCourseIds.length > 0 ? this.getQuizCounts(elementsCriteria) : []
+      const promise2: Promise<any> | [] =
+        nonElementsCriteria.courseIds.length > 0
+          ? this.getQuizCounts(nonElementsCriteria)
+          : []
+
+      const [result1, result2] = await Promise.all([promise1, promise2])
       result = result1.concat(result2)
     }
 
