b1912a852188137fdcc23dd818f190742e665fba
Meri <meri.rajamaki@gmail.com>
2020-05-22 14:08:03 +0300

    danish options added

--- packages/moocfi-quizzes/src/utils/languages/index.ts
+++ packages/moocfi-quizzes/src/utils/languages/index.ts
@@ -9,6 +9,7 @@ import italianLabels from "./italian_options"
 import latvianLabels from "./latvian_options"
 import hungarianLabels from "./hungarian_options"
 import dutchLabels from "./dutch_options"
+import danishLabels from "./danish_options"
 import { QuizPointsGrantingPolicy } from "../../modelTypes"
 
 export type GeneralLabels = {
@@ -153,4 +154,5 @@ export const languageOptions: LanguageLabels = {
   lv_LV: latvianLabels,
   hu_HU: hungarianLabels,
   nl_NL: dutchLabels,
+  da_DA: danishLabels,
 }
