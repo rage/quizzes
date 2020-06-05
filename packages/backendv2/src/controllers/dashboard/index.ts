8ed1abbe1ce8c4da04063eb7115cb010acacbe92
Perkoila Kalle M <kalle.perkoila@helsinki.fi>
2020-06-05 13:28:47 +0300

    get all courses for dashboard

--- packages/backendv2/src/controllers/dashboard/index.ts
+++ packages/backendv2/src/controllers/dashboard/index.ts
@@ -1,13 +1,17 @@
 import Router from "koa-router"
 import { CustomContext, CustomState } from "../../types"
-import { Quiz } from "../../models/"
+import { Course, Quiz } from "../../models/"
 import accessControl from "../../middleware/access_control"
 
 const dashboard = new Router<CustomState, CustomContext>({
   prefix: "/dashboard",
-}).get("/:quizId", accessControl({ administator: true }), async ctx => {
-  const quizId = ctx.params.quizId
-  ctx.body = await Quiz.getQuizById(quizId)
 })
+  .get("/quizzes/:quizId", accessControl({ administator: true }), async ctx => {
+    const quizId = ctx.params.quizId
+    ctx.body = await Quiz.getQuizById(quizId)
+  })
+  .get("/courses", accessControl({ administator: true }), async ctx => {
+    ctx.body = await Course.getAll()
+  })
 
 export default dashboard
