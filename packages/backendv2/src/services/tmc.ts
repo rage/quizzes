b02f09c0c1eebcf3884287d6a6ad206ef2c18f65
Perkoila Kalle M <kalle.perkoila@helsinki.fi>
2020-06-05 22:11:40 +0300

    tmc url doesn't need to be environment variable

--- packages/backendv2/src/services/tmc.ts
+++ packages/backendv2/src/services/tmc.ts
@@ -1,17 +1,15 @@
 import axios from "axios"
-import env from "../util/environment"
 import { UserInfo } from "../types"
 
 export const getCurrentUserDetails = async (
   accessToken: string,
 ): Promise<UserInfo> => {
   const res = await axios.get(
-    `${env.TMC_BASE_URL}/api/v8/users/current?show_user_fields=true`,
+    `https://tmc.mooc.fi/api/v8/users/current?show_user_fields=true`,
     {
       headers: { Authorization: `Bearer ${accessToken}` },
     },
   )
-
   const userInfo = res.data
   return userInfo
 }
