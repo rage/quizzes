eb7f41e692df9bb9b8a5b76da08a8c98add6506d
Meri <meri.rajamaki@gmail.com>
2020-06-11 22:08:00 +0300

    hu options fixes

--- packages/moocfi-quizzes/src/utils/languages/hungarian_options.ts
+++ packages/moocfi-quizzes/src/utils/languages/hungarian_options.ts
@@ -3,112 +3,112 @@ import { SingleLanguageLabels } from "./index"
 const hungarianLabels: SingleLanguageLabels = {
   essay: {
     exampleAnswerLabel: "Példaválasz",
-    userAnswerLabel: "Az Ön válasza",
+    userAnswerLabel: "A válaszod",
     currentNumberOfWordsLabel: "Szavak száma",
-    textFieldLabel: "Az Ön válasza",
+    textFieldLabel: "A válaszod",
     conformToLimitsToSubmitLabel:
-      "Ahhoz, hogy el tudja küldeni válaszát, győződjön meg arról, hogy nem lépte túl a szókorlátot",
+      "Ahhoz, hogy el tudd küldeni a választ, győződj meg róla, hogy nem lépted túl a szókorlátot",
     wordLimitsGuidance: (min, max) => {
       if (!min && !max) {
         return ""
       }
       if (!min) {
-        return `Válasza legfeljebb ${max} szót tartalmazhat`
+        return `A válasz legfeljebb ${max} szót tartalmazhat`
       }
 
       if (!max) {
-        return `Válaszának legalább ${min} szót kell tartalmaznia`
+        return `A válasznak legalább ${min} szót kell tartalmaznia`
       }
-      return `Válaszának legalább ${min} szót kell tartalmaznia, de nem lehet hosszabb ${max} szónál`
+      return `A válasznak legalább ${min} szót kell tartalmaznia, de nem lehet hosszabb ${max} szónál`
     },
   },
   open: {
     placeholder: "Válasz",
-    userAnswerLabel: "Az Ön válasza",
-    feedbackForSuccess: "Válasza helyes",
-    feedbackForFailure: "Válasza helytelen",
+    userAnswerLabel: "A válaszod",
+    feedbackForSuccess: "A válasz helyes",
+    feedbackForFailure: "A válasz helytelen",
   },
   peerReviews: {
     loadingLabel: "Betöltés",
     chooseButtonLabel: "Kiválasztás",
     unselectButtonLabel: "Kiválasztás törlése",
-    chooseEssayInstruction: "Válassza ki az egyik opciót értékelésre",
-    chosenEssayInstruction: "Ellenőrizze a kiválasztott választ",
-    givenPeerReviewsLabel: "Partneri értékelés készítése",
+    chooseEssayInstruction: "Válaszd ki az egyik opciót értékelésre",
+    chosenEssayInstruction: "Ellenőrizd a kiválasztott választ",
+    givenPeerReviewsLabel: "Társértékelés készítése",
     noPeerAnswersAvailableLabel:
-      "Partneri értékeléshez nem áll rendelkezésre válasz",
+      "Társértékeléshez nem áll rendelkezésre válasz",
     reportAsInappropriateLabel: "Spam jelentése",
     submitPeerReviewLabel: "Értékelés küldése",
-    peerReviewsCompletedInfo: "Elegendő partneri értékelést készített",
+    peerReviewsCompletedInfo: "Elegendő társértékelést készítettél",
     giveExtraPeerReviews:
-      "Elkészítette a szükséges számú partneri értékelést. Ha több partneri értékelést készít, válaszát gyorsabban értékeljük!",
+      "Elkészítetted a szükséges számú társértékelést. Ha több társértékelést készítesz, válaszod gyorsabban értékeljük!",
     giveExtraPeerReviewsQuizConfirmed:
-      "További partneri értékeléseket is készíthet, hogy segítsen másoknak",
+      "További társértékeléseket is készíthetsz, hogy segíts másoknak",
     displayPeerReview: "Készítsen partneri értékelést",
     hidePeerReviewLabel: "Elrejtés",
     essayQuestionAnswerTextBoxLabel: "Értékelés írása",
     optionLabel: "Opció",
-    answerRejected: "Válaszát elutasították",
-    answerFlaggedAsSpam: "Válaszát spamként jelentették",
-    answerConfirmed: "Válaszát elfogadták!",
-    manualReview: "Válaszát a tanfolyam munkatársai ellenőrzik",
+    answerRejected: "Válaszod elutasították",
+    answerFlaggedAsSpam: "Válaszod spamként jelentették",
+    answerConfirmed: "Válaszod elfogadták",
+    manualReview: "Válaszod a tanfolyam munkatársai ellenőrzik",
     peerReviewGroupTitle: "Szakértői vélemény kérdései",
     peerReviewLikertDetails:
       "Az egyes állításokat értékelje 1-5 skálán. 1 azt jelenti, hogy nem ért egyet, 5 azt jelenti, hogy határozottan egyetért.",
   },
   receivedPeerReviews: {
     averageOfGradesLabel: "A kapott értékelések osztályzatainak átlaga:",
-    detailedViewLabel: "A válaszára kapott összes értékelés",
+    detailedViewLabel: "A válaszodra kapott összes értékelés",
     errorLabel:
-      "Hiba történt a beérkezett partneri értékelések megjelenítésekor. Kérjük, próbálja újra betölteni az oldalt.",
-    loadingLabel: "A kapott partneri értékelések betöltése...",
-    noPeerReviewsReceivedlabel: "Válaszát még egy partner sem értékelte",
+      "Hiba történt a beérkezett társértékelések megjelenítésekor. Kérjük, próbáld meg újra betölteni az oldalt.",
+    loadingLabel: "A kapott társértékelések betöltése...",
+    noPeerReviewsReceivedlabel: "Válaszod még egy társ sem értékelte",
     noSupportForQuestionTypeLabel:
-      "Ez a fajta partneri értékelési kérdés nem támogatott",
+      "Ez a fajta társértékelési kérdés nem támogatott	",
     numberOfPeerReviewsText: n =>
-      `Válaszát ${n} számú partner értékelte${n > 1 ? "s" : ""}.`,
-    summaryViewLabel: "A kapott partneri értékelések:",
-    toggleButtonExpandLabel: "Az összes partneri értékelések megjelenítése",
-    toggleButtonShrinkLabel: "A partneri értékelések elrejtése",
-    peerReviewLabel: "Partneri értékelés",
-    peerReviewReceived: "Új partneri értékelés érkezett",
+      `Válaszod ${n} számú társ értékelte.`,
+    summaryViewLabel: "A kapott társértékelések:",
+    toggleButtonExpandLabel: "Az összes társértékelés megjelenítése",
+    toggleButtonShrinkLabel: "A társértékelések elrejtése",
+    peerReviewLabel: "Társértékelés",
+    peerReviewReceived: "Új társértékelés érkezett",
     peerReviewReceivedFor: (title: string) =>
-      `Az ${title} feladat esetében új partneri értékelés érkezett.`,
+      `Az ${title} feladat esetében új társértékelés érkezett`,
   },
   unsupported: {
     notSupportedInsert: (itemType: string) =>
       `Az '${itemType}' típusú kérdés nem támogatott.`,
   },
   multipleChoice: {
-    selectCorrectAnswerLabel: "Válassza ki a helyes választ",
+    selectCorrectAnswerLabel: "Válaszd ki a helyes választ",
     chooseAllSuitableOptionsLabel:
-      "Kérjük, jelölje be az összes megfelelő választ",
+      "Kérjük, jelöld be az összes megfelelő választ",
     answerCorrectLabel: "Helyes",
     answerIncorrectLabel: "Helytelen",
   },
   stage: {
     answerStageLabel: "A feladat megválaszolása",
-    givingPeerReviewsStageLabel: "Partneri értékelése készítése",
-    receivingPeerReviewsStageLabel: "Partneri értékelések fogadása",
+    givingPeerReviewsStageLabel: "Társértékelések készítése",
+    receivingPeerReviewsStageLabel: "Társértékelések fogadása",
     evaluationStageLabel: "Osztályzatra vár",
   },
   general: {
-    pastDeadline: "Erre a feladatra már nem áll módjában válaszolni",
+    pastDeadline: "Erre a feladatra már nem áll módodban válaszolni",
     answerMissingBecauseQuizModifiedLabel:
-      "A kérdésre nem érkezett válasz. A tesztet a válaszát követően valószínűleg módosították.",
+      "A kérdésre nem érkezett válasz. A tesztet a válasz elküldését követően valószínűleg módosították.",
     submitButtonLabel: "Küldés",
     errorLabel: "Hiba",
-    loginToViewPromptLabel: "Jelentkezzen be a feladat megtekintéséhez",
-    loginToAnswerPromptLabel: "Jelentkezzen be a feladat megválaszolásához",
+    loginToViewPromptLabel: "Jelentkezz be a feladat megtekintéséhez",
+    loginToAnswerPromptLabel: "Jelentkezz be a feladat megválaszolásához",
     loadingLabel: "Betöltés",
     answerCorrectLabel: "A válasz helyes",
-    alreadyAnsweredLabel: "Már válaszolt erre a kérdésre",
+    alreadyAnsweredLabel: "Már válaszoltál erre a kérdésre",
     answerIncorrectLabel: "A válasz helytelen",
     kOutOfNCorrect: (k, n) => `${k}/${n} válasz helyes`,
     pointsAvailableLabel: "A feladatra kapott pontok",
     pointsReceivedLabel: "Elért pontszám",
     incorrectSubmitWhileTriesLeftLabel:
-      "A válasz nem volt teljesen helyes. Kérjük, próbálja meg újra!",
+      "A válasz nem volt teljesen helyes. Kérjük, próbáld meg újra!",
     triesRemainingLabel: "Fennmaradó próbálkozások",
     quizLabel: "Tesztkérdések",
     pointsLabel: "Pontszám",
@@ -118,7 +118,7 @@ const hungarianLabels: SingleLanguageLabels = {
     pointsGrantingPolicyInformer: policy => {
       switch (policy) {
         case "grant_only_when_answer_fully_correct":
-          return "Csak teljesen helyes válasz esetén kap pontot."
+          return "Csak teljesen helyes válasz esetén kapsz pontot."
         case "grant_whenever_possible":
           return ""
         default:
@@ -129,19 +129,19 @@ const hungarianLabels: SingleLanguageLabels = {
     unanswered: "meg nem válaszolt",
     rejected: "Elutasított válasz, próbálkozzon újra",
     progressUpdated: "A tanfolyam során elért eredmények frissültek",
-    answerConfirmed: "Válaszát megerősítették!",
+    answerConfirmed: "Válaszod megerősítették!",
     answerConfirmedFor: (title: string) =>
-      `Az ${title} feladatra adott válaszát megerősítették!`,
-    courseCompleted: "Elvégezte a tanfolyamot!",
+      `Az ${title} feladatra adott válaszod megerősítették!`,
+    courseCompleted: "Elvégezted a tanfolyamot!",
   },
   error: {
-    submitFailedError: "Válasza nem küldhető el. Kérjük, próbálja meg később.",
+    submitFailedError: "A válasz nem küldhető el. Kérjük, próbáld meg később.",
     quizLoadFailedError: "Nem tölthető be a feladat.",
     progressFetchError:
-      "Nem sikerült beolvasni a tanfolyam során elért eredményeket. Kérjük, próbálja meg később.",
+      "Nem sikerült beolvasni a tanfolyam során elért eredményeket. Kérjük, próbáld meg később.",
     submitSpamFlagError: "Nem sikerült a spam jelentése",
     fetchReviewCandidatesError:
-      "Probléma merül fel a válaszok partneri értékelés céljából való visszakeresése során. Kérjük, próbálja meg később.",
+      "Probléma merül fel a válaszok társértékelés céljából való visszakeresése során. Kérjük, próbáld meg később.",
   },
 }
 
