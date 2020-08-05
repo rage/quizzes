00ebbe9f7e11090d1a0d3df0103c59141ce32bd0
Joel Hassan <joel.nhn@gmail.com>
2020-08-05 14:06:25 +0300

    exp

--- packages/moocfi-quizzes/src/utils/languages/index.ts
+++ packages/moocfi-quizzes/src/utils/languages/index.ts
@@ -18,6 +18,7 @@ import czechLabels from "./czech_options"
 import polishLabels from "./polish_options"
 import malteseLabels from "./maltese_options"
 import spanishLabels from "./spanish_options"
+import slovenianLabels from "./slovenian_options"
 import { QuizPointsGrantingPolicy } from "../../modelTypes"
 
 export type GeneralLabels = {
@@ -174,4 +175,5 @@ export const languageOptions: LanguageLabels = {
   pl_PL: polishLabels,
   mt_MT: malteseLabels,
   es_ES: spanishLabels,
+  sl_SL: slovenianLabels,
 }
