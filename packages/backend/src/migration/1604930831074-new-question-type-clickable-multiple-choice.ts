074e32a2721d51c40651f552a36d5c079b58b744
root <root@DESKTOP-QG1DJ2E.localdomain>
2020-11-09 16:47:28 +0200

    add migration for clickable-multiple-choice

---
+++ packages/backend/src/migration/1604930831074-new-question-type-clickable-multiple-choice.ts
@@ -0,0 +1,35 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class newQuestionTypeClickableMultipleChoice1604930831074 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+          await queryRunner.query(
+            // tslint:disable-next-line:max-line-length
+            "create type quiz_item_type_enum_new as enum ('open','scale','essay','multiple-choice','checkbox','research-agreement','feedback','custom-frontend-accept-data','multiple-choice-dropdown','clickable-multiple-choice')",
+          )
+          await queryRunner.query(
+            // tslint:disable-next-line:max-line-length
+            "alter table quiz_item alter column type set data type quiz_item_type_enum_new using (status::text::quiz_item_type_enum_new)",
+          )
+          await queryRunner.query("drop type quiz_item_type_enum")
+          await queryRunner.query(
+            "alter type quiz_item_type_enum_new rename to quiz_item_type_enum",
+          )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(
+            // tslint:disable-next-line:max-line-length
+            "create type quiz_item_type_enum_new as enum ('open','scale','essay','multiple-choice','checkbox','research-agreement','feedback','custom-frontend-accept-data','multiple-choice-dropdown')",
+          )
+          await queryRunner.query(
+            // tslint:disable-next-line:max-line-length
+            "alter table quiz_item alter column type set data type quiz_item_type_enum_new using (status::text::quiz_item_type_enum_new)",
+          )
+          await queryRunner.query("drop type quiz_item_type_enum")
+          await queryRunner.query(
+            "alter type quiz_item_type_enum_new rename to quiz_item_type_enum",
+          )
+    }
+
+}
