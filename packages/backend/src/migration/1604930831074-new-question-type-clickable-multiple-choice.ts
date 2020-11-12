f49c8d2e3f24e613022a762fc976e7398061527c
Henrik Nygren <nygrenh@gmail.com>
2020-11-12 11:57:32 +0200

    Fix migration

--- packages/backend/src/migration/1604930831074-new-question-type-clickable-multiple-choice.ts
+++ packages/backend/src/migration/1604930831074-new-question-type-clickable-multiple-choice.ts
@@ -5,7 +5,7 @@ export class NewQuestionTypeClickableMultipleChoice1604930831074
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
       // tslint:disable-next-line:max-line-length
-      "drop materialized view reaktor.quiz_item cascade",
+      "drop materialized view if exists reaktor.quiz_item cascade",
     )
 
     await queryRunner.query(
@@ -20,9 +20,56 @@ export class NewQuestionTypeClickableMultipleChoice1604930831074
     await queryRunner.query(
       "alter type quiz_item_type_enum_new rename to quiz_item_type_enum",
     )
+    await queryRunner.query(
+      `
+      CREATE materialized VIEW reaktor.quiz_item AS
+      SELECT
+          *
+      FROM
+          quiz_item
+      WHERE
+          quiz_id IN (
+              SELECT
+                  id
+              FROM
+                  reaktor.quiz
+          )
+      `,
+    )
+
+    await queryRunner.query(`CREATE INDEX ON reaktor.quiz_item (id)`)
+
+    await queryRunner.query(
+      `
+      CREATE materialized VIEW reaktor.quiz_option AS
+      SELECT
+          *
+      FROM
+          quiz_option
+      WHERE
+          quiz_item_id IN (
+              SELECT
+                  id
+              FROM
+                  reaktor.quiz_item
+          )
+      `,
+    )
+
+    await queryRunner.query(`CREATE INDEX ON reaktor.quiz_option (id)`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      // tslint:disable-next-line:max-line-length
+      "drop materialized view if exists reaktor.quiz_item cascade",
+    )
+
+    await queryRunner.query(
+      // tslint:disable-next-line:max-line-length
+      "drop materialized view if exists reaktor.quiz_item cascade",
+    )
+
     await queryRunner.query(
       // tslint:disable-next-line:max-line-length
       "create type quiz_item_type_enum_new as enum ('open','scale','essay','multiple-choice','checkbox','research-agreement','feedback','custom-frontend-accept-data','multiple-choice-dropdown')",
@@ -35,5 +82,42 @@ export class NewQuestionTypeClickableMultipleChoice1604930831074
     await queryRunner.query(
       "alter type quiz_item_type_enum_new rename to quiz_item_type_enum",
     )
+    await queryRunner.query(
+      `
+      CREATE materialized VIEW reaktor.quiz_item AS
+      SELECT
+          *
+      FROM
+          quiz_item
+      WHERE
+          quiz_id IN (
+              SELECT
+                  id
+              FROM
+                  reaktor.quiz
+          )
+      `,
+    )
+
+    await queryRunner.query(`CREATE INDEX ON reaktor.quiz_item (id)`)
+
+    await queryRunner.query(
+      `
+      CREATE materialized VIEW reaktor.quiz_option AS
+      SELECT
+          *
+      FROM
+          quiz_option
+      WHERE
+          quiz_item_id IN (
+              SELECT
+                  id
+              FROM
+                  reaktor.quiz_item
+          )
+      `,
+    )
+
+    await queryRunner.query(`CREATE INDEX ON reaktor.quiz_option (id)`)
   }
 }
