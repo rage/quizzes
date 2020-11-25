23bb668a3956c645fc4f447ef7fda98a4c82183f
Joel <joel.nhn@gmail.com>
2020-11-25 20:48:14 +0200

    add endpoint for searching manual review answers

--- packages/backendv2/src/controllers/dashboard/index.ts
+++ packages/backendv2/src/controllers/dashboard/index.ts
@@ -194,6 +194,21 @@ const dashboard = new Router<CustomState, CustomContext>({
     )
   })
 
+  .post("/answers/:quizId/manual-review", accessControl(), async ctx => {
+    const quizId = ctx.params.quizId
+    const courseId = await getCourseIdByQuizId(quizId)
+    await checkAccessOrThrow(ctx.state.user, courseId, "view")
+    const { page, size, order } = ctx.request.query
+    const { searchQuery } = ctx.request.body
+    ctx.body = await QuizAnswer.getPaginatedManualReviewBySearchQuery(
+      quizId,
+      page,
+      size,
+      order,
+      searchQuery,
+    )
+  })
+
   .post("/quizzes/:quizId/download-quiz-info", accessControl(), async ctx => {
     const quizId = ctx.params.quizId
     const quizName = ctx.request.body.quizName
