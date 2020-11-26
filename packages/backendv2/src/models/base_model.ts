51ce1a6fe1839ff42181a9a89af767f40238fbbc
Henrik Nygren <nygrenh@gmail.com>
2020-11-26 22:01:58 +0200

    Set timezone to utc

--- packages/backendv2/src/models/base_model.ts
+++ packages/backendv2/src/models/base_model.ts
@@ -1,10 +1,10 @@
 import { Model } from "objection"
 
 class BaseModel extends Model {
-  public updatedAt!: Date
+  public updatedAt!: string
   async $beforeUpdate(opt: any, queryContext: any) {
     await super.$beforeUpdate(opt, queryContext)
-    this.updatedAt = new Date()
+    this.updatedAt = new Date().toISOString()
   }
 }
 
