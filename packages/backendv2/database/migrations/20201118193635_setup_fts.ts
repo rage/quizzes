3f71b6bc36f07a3c7f453ff3d06f82b525cddfc1
Joel <joel.nhn@gmail.com>
2020-12-02 21:02:46 +0200

    modify fts migration

--- packages/backendv2/database/migrations/20201118193635_setup_fts.ts
+++ packages/backendv2/database/migrations/20201118193635_setup_fts.ts
@@ -1,7 +1,7 @@
 import * as Knex from "knex"
 
 const addAnswerIndex = `
-CREATE INDEX idx_fts_answer ON public.quiz_item_answer USING gin (to_tsvector('english', coalesce(text_data, '')));
+CREATE INDEX IF NOT EXISTS idx_fts_answer ON public.quiz_item_answer USING gin (to_tsvector('english', coalesce(text_data, '')));
 `
 
 const removeAnswerIndex = `
@@ -9,9 +9,7 @@ const removeAnswerIndex = `
 `
 
 export async function up(knex: Knex): Promise<any> {
-  if (process.env.NODE_ENV !== "test") {
-    await knex.schema.raw(addAnswerIndex)
-  }
+  await knex.schema.raw(addAnswerIndex)
 }
 
 export async function down(knex: Knex): Promise<any> {
