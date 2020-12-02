693bb25abde2eb3e0b1b2d109d3a15794c4b0de3
Joel <joel.nhn@gmail.com>
2020-12-02 22:43:14 +0200

    add tests for manual review answer search

--- packages/backendv2/database/seeds/searchableQuizAnswers.ts
+++ packages/backendv2/database/seeds/searchableQuizAnswers.ts
@@ -2,6 +2,18 @@ import * as Knex from "knex"
 
 export async function seed(knex: Knex): Promise<any> {
   await knex.raw("? ON CONFLICT (id) DO NOTHING", [
+    knex("quiz_answer").insert([
+      {
+        id: "4541181b-e667-4970-97be-07bd360fc895",
+        quiz_id: "4bf4cf2f-3058-4311-8d16-26d781261af7",
+        user_id: 1234,
+        language_id: "xy_YZ",
+        status: "manual-review",
+      },
+    ]),
+  ])
+
+  await knex.raw("? ON CONFLICT (id) DO NOTHING", [
     knex("quiz_item_answer").insert([
       {
         id: "9b7ae31b-30d5-4bdb-8cab-f613624a6194",
@@ -32,6 +44,12 @@ export async function seed(knex: Knex): Promise<any> {
         quiz_answer_id: "0cb3e4de-fc11-4aac-be45-06312aa4677c",
         quiz_item_id: "707195a3-aafe-4c06-bf23-854e54e084db",
         text_data: "lorem ipsum something something.",
+      },
+      {
+        id: "b4640ca7-216e-4b1c-8798-1d9c617471f1",
+        quiz_answer_id: "4541181b-e667-4970-97be-07bd360fc895",
+        quiz_item_id: "707195a3-aafe-4c06-bf23-854e54e084db",
+        text_data: "linked to an answer queued for manual review",
       },
     ]),
   ])
--- packages/backendv2/tests/textSearch.test.ts
+++ packages/backendv2/tests/textSearch.test.ts
@@ -31,7 +31,7 @@ afterAll(async () => {
   await knex.destroy()
 })
 
-describe("dashboard: searching for answers based on text content should", () => {
+describe("dashboard: searching for all answers based on text content should", () => {
   beforeAll(async () => {
     await safeSeed({
       directory: "./database/seeds",
@@ -113,7 +113,7 @@ describe("dashboard: searching for answers based on text content should", () => 
         .expect(200, done)
     })
 
-    test("return 3 answers when 3 asnwers match search query", done => {
+    test("return 3 answers when 3 answers match search query", done => {
       request(app.callback())
         .post(
           "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/all?page=0",
@@ -140,5 +140,134 @@ describe("dashboard: searching for answers based on text content should", () => 
         })
         .expect(200, done)
     })
+  })
+})
+
+describe("dashboard: searching for manual review answers based on text content should", () => {
+  beforeAll(async () => {
+    await safeSeed({
+      directory: "./database/seeds",
+    })
+  })
+
+  afterAll(async () => {
+    await safeClean()
+  })
+
+  beforeEach(async () => {
+    nock("https://tmc.mooc.fi")
+      .get("/api/v8/users/current?show_user_fields=true")
+      .reply(function() {
+        const auth = this.req.headers.authorization
+        if (auth === "Bearer pleb_token") {
+          return [
+            200,
+            {
+              id: 6666,
+              administrator: false,
+            } as UserInfo,
+          ]
+        }
+        if (auth === "Bearer admin_token") {
+          return [
+            200,
+            {
+              administrator: true,
+            } as UserInfo,
+          ]
+        }
+      })
+  })
+
+  test("respond with 401 if invalid credentials", done => {
+    request(app.callback())
+      .post(
+        "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review",
+      )
+      .set("Authorization", `bearer BAD_TOKEN`)
+      .set("Accept", "application/json")
+      .expect(401, done)
+  })
+
+  test("respond with 403 if insufficient privilege", done => {
+    request(app.callback())
+      .post(
+        "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review",
+      )
+      .set("Authorization", `bearer PLEB_TOKEN`)
+      .set("Accept", "application/json")
+      .expect(403, done)
+  })
+
+  test("throw bad request if no search query provided", done => {
+    request(app.callback())
+      .post(
+        "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review",
+      )
+      .set("Authorization", `bearer ADMIN_TOKEN`)
+      .set("Accept", "application/json")
+      .expect(response => {
+        const received: BadRequestError = response.body
+        expect(received.message).toEqual("No search query provided.")
+      })
+      .expect(400, done)
+  })
+
+  describe("on successful search", () => {
+    test("return 200", done => {
+      request(app.callback())
+        .post(
+          "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review",
+        )
+        .set("Authorization", `bearer ADMIN_TOKEN`)
+        .set("Accept", "application/json")
+        .send({ searchQuery: "test" })
+        .expect(response => {
+          console.log(response.body)
+        })
+        .expect(200, done)
+    })
+
+    test("return 0 matches when non exist", done => {
+      request(app.callback())
+        .post(
+          "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review",
+        )
+        .set("Authorization", `bearer ADMIN_TOKEN`)
+        .set("Accept", "application/json")
+        .send({ searchQuery: "blaadi blaa" })
+        .expect(response => {
+          expect(response.body.total).toEqual(0)
+        })
+        .expect(200, done)
+    })
+
+    test("return 1 answer requiring manual review when only 1 matches the query", done => {
+      request(app.callback())
+        .post(
+          "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review",
+        )
+        .set("Authorization", `bearer ADMIN_TOKEN`)
+        .set("Accept", "application/json")
+        .send({ searchQuery: "linked to" })
+        .expect(response => {
+          expect(response.body.total).toEqual(1)
+        })
+        .expect(200, done)
+    })
+
+    // test("return 0 answers when no content matching search query ", done => {
+    //   request(app.callback())
+    //     .post(
+    //       "/api/v2/dashboard/answers/4bf4cf2f-3058-4311-8d16-26d781261af7/manual-review?page=0",
+    //     )
+    //     .set("Authorization", `bearer ADMIN_TOKEN`)
+    //     .set("Accept", "application/json")
+    //     .send({ searchQuery: "king of kings" })
+    //     .expect(response => {
+    //       expect(response.body.total).toEqual(0)
+    //     })
+    //     .expect(200, done)
+    // })
   })
 })
