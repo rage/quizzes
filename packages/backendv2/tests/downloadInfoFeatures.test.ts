355707b78f98f9a05928822d8d5b7e70ec54664e
Joel <joel.nhn@gmail.com>
2021-01-22 14:17:52 +0200

    try again

--- packages/backendv2/tests/downloadInfoFeatures.test.ts
+++ packages/backendv2/tests/downloadInfoFeatures.test.ts
@@ -11,82 +11,8 @@ import redis from "../config/redis"
 afterAll(async () => {
   await safeClean()
   await knex.destroy()
-  await redis.client?.quit()
-})
-
-afterEach(async () => {
   await redis.client?.flushall()
-})
-
-describe("downloading info when redis unavailable", () => {
-  const tmp = redis.client
-  beforeEach(() => {
-    redis.client = undefined
-    nock("https://tmc.mooc.fi")
-      .get("/api/v8/users/current?show_user_fields=true")
-      .reply(function() {
-        const auth = this.req.headers.authorization
-        if (auth === "Bearer admin_token") {
-          return [
-            200,
-            {
-              administrator: true,
-              username: "admin",
-            } as UserInfo,
-          ]
-        }
-      })
-  })
-
-  afterAll(async () => {
-    redis.client = tmp
-  })
-
-  test("should throw a BadRequestError when token not generated on quiz info download", done => {
-    const quizId = "4bf4cf2f-3058-4311-8d16-26d781261af7"
-    request(app.callback())
-      .post(`/api/v2/dashboard/quizzes/${quizId}/download-quiz-info`)
-      .set("Authorization", `bearer ADMIN_TOKEN`)
-      .send({
-        courseId: "46d7ceca-e1ed-508b-91b5-3cc8385fa44b",
-      })
-      .expect(400)
-      .expect(response => {
-        const received: BadRequestError = response.body
-        expect(received.message).toEqual("Failed to generate download token.")
-      })
-      .end(done)
-  })
-  test("should throw a BadRequestError when token not generated on peer review info download", done => {
-    const quizId = "4bf4cf2f-3058-4311-8d16-26d781261af7"
-    request(app.callback())
-      .post(`/api/v2/dashboard/quizzes/${quizId}/download-peerreview-info`)
-      .set("Authorization", `bearer ADMIN_TOKEN`)
-      .send({
-        courseId: "46d7ceca-e1ed-508b-91b5-3cc8385fa44b",
-      })
-      .expect(400)
-      .expect(response => {
-        const received: BadRequestError = response.body
-        expect(received.message).toEqual("Failed to generate download token.")
-      })
-      .end(done)
-  })
-  test("should throw a BadRequestError when token not generated on answer info download", done => {
-    const quizId = "4bf4cf2f-3058-4311-8d16-26d781261af7"
-    request(app.callback())
-      .post(`/api/v2/dashboard/quizzes/${quizId}/download-answer-info`)
-      .set("Authorization", `bearer ADMIN_TOKEN`)
-      .send({
-        courseId: "46d7ceca-e1ed-508b-91b5-3cc8385fa44b",
-      })
-      .expect(400)
-      .expect(response => {
-        const received: BadRequestError = response.body
-        expect(received.message).toEqual("Failed to generate download token.")
-      })
-      .end(done)
-  })
+  await redis.client?.quit()
 })
 
 describe("dashboard - courses: downloading quiz info should", () => {
@@ -322,6 +248,77 @@ describe("dashboard: downloading answer info should", () => {
         expect(downloadUrl).toInclude(
           `/download/download-answer-info/${quizId}?&downloadToken=`,
         )
+      })
+      .end(done)
+  })
+})
+
+describe("downloading info when redis unavailable", () => {
+  const tmp = redis.client
+  beforeEach(() => {
+    redis.client = undefined
+    nock("https://tmc.mooc.fi")
+      .get("/api/v8/users/current?show_user_fields=true")
+      .reply(function() {
+        const auth = this.req.headers.authorization
+        if (auth === "Bearer admin_token") {
+          return [
+            200,
+            {
+              administrator: true,
+              username: "admin",
+            } as UserInfo,
+          ]
+        }
+      })
+  })
+
+  afterAll(async () => {
+    redis.client = tmp
+  })
+
+  test("should throw a BadRequestError when token not generated on quiz info download", done => {
+    const quizId = "4bf4cf2f-3058-4311-8d16-26d781261af7"
+    request(app.callback())
+      .post(`/api/v2/dashboard/quizzes/${quizId}/download-quiz-info`)
+      .set("Authorization", `bearer ADMIN_TOKEN`)
+      .send({
+        courseId: "46d7ceca-e1ed-508b-91b5-3cc8385fa44b",
+      })
+      .expect(400)
+      .expect(response => {
+        const received: BadRequestError = response.body
+        expect(received.message).toEqual("Failed to generate download token.")
+      })
+      .end(done)
+  })
+  test("should throw a BadRequestError when token not generated on peer review info download", done => {
+    const quizId = "4bf4cf2f-3058-4311-8d16-26d781261af7"
+    request(app.callback())
+      .post(`/api/v2/dashboard/quizzes/${quizId}/download-peerreview-info`)
+      .set("Authorization", `bearer ADMIN_TOKEN`)
+      .send({
+        courseId: "46d7ceca-e1ed-508b-91b5-3cc8385fa44b",
+      })
+      .expect(400)
+      .expect(response => {
+        const received: BadRequestError = response.body
+        expect(received.message).toEqual("Failed to generate download token.")
+      })
+      .end(done)
+  })
+  test("should throw a BadRequestError when token not generated on answer info download", done => {
+    const quizId = "4bf4cf2f-3058-4311-8d16-26d781261af7"
+    request(app.callback())
+      .post(`/api/v2/dashboard/quizzes/${quizId}/download-answer-info`)
+      .set("Authorization", `bearer ADMIN_TOKEN`)
+      .send({
+        courseId: "46d7ceca-e1ed-508b-91b5-3cc8385fa44b",
+      })
+      .expect(400)
+      .expect(response => {
+        const received: BadRequestError = response.body
+        expect(received.message).toEqual("Failed to generate download token.")
       })
       .end(done)
   })
