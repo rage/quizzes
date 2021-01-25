20534b98affa6dafc3204b741887a2509b8700de
Joel <joel.nhn@gmail.com>
2021-01-25 15:10:43 +0200

    log redis

--- packages/backendv2/config/redis.ts
+++ packages/backendv2/config/redis.ts
@@ -6,6 +6,11 @@ if (process.env.REDIS_HOST && process.env.REDIS_PORT) {
     port: Number(process.env.REDIS_PORT),
     host: process.env.REDIS_HOST,
   })
+  if (client) {
+    console.log(
+      `Redis client initialised and running on ${process.env.REDIS_HOS}:${process.env.REDIS_PORT}`,
+    )
+  }
 }
 
 export default { client }
