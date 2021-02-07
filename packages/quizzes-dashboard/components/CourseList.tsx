4a8c43075a66a53bbfb6fb9e602d1c91827509b2
Joel <joel.nhn@gmail.com>
2021-02-07 17:33:53 +0200

    make course links open on new tabs/windows

--- packages/quizzes-dashboard/components/CourseList.tsx
+++ packages/quizzes-dashboard/components/CourseList.tsx
@@ -64,7 +64,7 @@ const CourseList = ({ data, error }: CourseListProps) => {
           variant="outlined"
           select
           value={sortBy}
-          onChange={event => setSortBy(event.target.value)}
+          onChange={(event) => setSortBy(event.target.value)}
         >
           <MenuItem value="title">Alphabetical</MenuItem>
           <MenuItem value="createdAt">Created at</MenuItem>
@@ -74,7 +74,7 @@ const CourseList = ({ data, error }: CourseListProps) => {
           variant="outlined"
           select
           value={sortOrder}
-          onChange={event => setSortOrder(event.target.value)}
+          onChange={(event) => setSortOrder(event.target.value)}
         >
           <MenuItem value="asc">
             {sortBy === "title" ? "[A - Z]" : "Oldest first"}
@@ -84,13 +84,13 @@ const CourseList = ({ data, error }: CourseListProps) => {
           </MenuItem>
         </OrderSelector>
       </OptionWrapper>
-      {courses.map(course => (
+      {courses.map((course) => (
         <Link key={course.id} href={`/courses/${course.id}/listing`}>
-          <CourseLink>
+          <a>
             <StyledCard key={course.id}>
               <CardContent>{course.title || course.id}</CardContent>
             </StyledCard>
-          </CourseLink>
+          </a>
         </Link>
       ))}
 
