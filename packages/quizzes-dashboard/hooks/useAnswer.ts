1d8cca5605de86fb9cd32dcaa1970ac4a42222a6
Joel <joel.nhn@gmail.com>
2021-02-18 16:37:49 +0200

    add answer fecth hook

---
+++ packages/quizzes-dashboard/hooks/useAnswer.ts
@@ -0,0 +1,14 @@
+import useSWR from "swr"
+import { getAnswerById } from "./../services/quizzes"
+
+const fetcher = (answerId: string) => getAnswerById(answerId)
+
+export const useAnswer = (answerId: string | undefined, token: string) => {
+  const { data, error } = useSWR(answerId ? [answerId, token] : null, fetcher)
+
+  return {
+    answer: data,
+    answerLoading: !error && data === undefined,
+    answerError: error,
+  }
+}
