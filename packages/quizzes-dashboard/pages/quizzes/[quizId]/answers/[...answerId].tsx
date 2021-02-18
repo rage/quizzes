bd4a92adeef075baa9254b063a99ed16c77b20da
Joel <joel.nhn@gmail.com>
2021-02-18 14:32:57 +0200

    enable tab routes on answerId page

--- packages/quizzes-dashboard/pages/quizzes/[quizId]/answers/[...answerId].tsx
+++ packages/quizzes-dashboard/pages/quizzes/[quizId]/answers/[...answerId].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import useBreadcrumbs from "../../../../hooks/useBreadcrumbs"
 import { useRouter } from "next/router"
 import usePromise from "react-use-promise"
@@ -11,12 +11,23 @@ import {
   TabTextError,
 } from "../../../../components/quizPages/TabHeaders"
 import SkeletonLoader from "../../../../components/Shared/SkeletonLoader"
+import { Tab, Tabs, Typography } from "@material-ui/core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChalkboard, faScroll } from "@fortawesome/free-solid-svg-icons"
+
+const Log = () => {
+  return <div>hhhh</div>
+}
 
 export const AnswerById = () => {
   const route = useRouter()
   const quizId = route.query.quizId?.toString()
+
   // When doing a full reload answerId is briefly undefined
-  const answerId = route.query.answerId?.toString()
+  const answerId = route.query.answerId && route.query.answerId[0].toString()
+  const pathname = `/quizzes/${quizId}/answers/${answerId}`
+
+  const [currentTab, setCurrentTab] = useState("overview")
 
   const [answerResponse, answerError] = usePromise(async () => {
     if (!answerId) {
@@ -31,6 +42,13 @@ export const AnswerById = () => {
     }
     return await fetchQuiz(quizId)
   }, [quizId])
+
+  /* for when tab is loaded through url*/
+  useEffect(() => {
+    if (route.query.answerId) {
+      setCurrentTab(route.query.answerId[1])
+    }
+  }, [route])
 
   useBreadcrumbs([
     { label: "Courses", as: "/", href: "/" },
@@ -75,7 +93,36 @@ export const AnswerById = () => {
   return (
     <>
       <TabText text="Singular answer" />
-      <AnswerCard answer={answerResponse} />
+      <Tabs
+        variant="fullWidth"
+        value={currentTab}
+        indicatorColor="primary"
+        textColor="primary"
+      >
+        <Tab
+          key="overview"
+          icon={<FontAwesomeIcon icon={faChalkboard} />}
+          value="overview"
+          label={<Typography>Overview</Typography>}
+          onClick={() => {
+            route.push(`${pathname}/overview`)
+          }}
+        />
+        <Tab
+          key="status-change-log"
+          icon={<FontAwesomeIcon icon={faScroll} />}
+          value="status-change-log"
+          label={<Typography>Status Change Log</Typography>}
+          onClick={() => {
+            route.push(`${pathname}/status-change-log`)
+          }}
+        />
+      </Tabs>
+      {currentTab === "overview" ? (
+        <AnswerCard answer={answerResponse} />
+      ) : (
+        <Log />
+      )}
     </>
   )
 }
