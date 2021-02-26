2fcdc03b1ca49988591540d724305eaa9e8c1f44
d471061c <cpastethat@gmail.com>
2021-02-26 16:30:24 +0200

    add course progress api

--- packages/backendv2/src/controllers/api.ts
+++ packages/backendv2/src/controllers/api.ts
@@ -2,6 +2,7 @@ import Router from "koa-router"
 import { CustomContext, CustomState } from "../types"
 import widget from "./widget"
 import dashboard from "./dashboard"
+import general from './general'
 
 const api = new Router<CustomState, CustomContext>({
   prefix: "/api/v2",
@@ -9,5 +10,6 @@ const api = new Router<CustomState, CustomContext>({
 
 api.use(widget.routes())
 api.use(dashboard.routes())
+api.use(general.routes())
 
 export default api
---
+++ packages/backendv2/src/controllers/general/index.ts
@@ -0,0 +1,31 @@
+import Router from 'koa-router'
+import { CustomContext, CustomState } from '../../types'
+import {
+    checkAccessOrThrow
+} from '../dashboard/util'
+import {
+    UserQuizState,
+    Quiz
+} from '../../models/'
+import accessControl from '../../middleware/access_control'
+
+const general = new Router<CustomState, CustomContext>({
+    prefix: "/general"
+})
+    .get("/course/:courseId/progress", accessControl(), async ctx => {
+        const courseId = ctx.params.courseId
+        const user = ctx.state.user
+        await checkAccessOrThrow(ctx.state.user, ctx.request.body.courseId, "view")
+        
+        let progress = []
+        
+        const result = await Quiz.getByCourseId(courseId)
+        for (const quiz of result) {
+            const quizProgress = await UserQuizState.getByUserAndQuiz(user.id, quiz.id)
+            progress.push(quizProgress)
+        }
+
+        ctx.body = progress
+    })
+
+export default general
