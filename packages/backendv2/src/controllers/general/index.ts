e8015890fbbc08a31a8d4d4eeb9883034872dd6f
d471061c <cpastethat@gmail.com>
2021-02-26 17:00:58 +0200

    add api for quiz-titles

--- packages/backendv2/src/controllers/general/index.ts
+++ packages/backendv2/src/controllers/general/index.ts
@@ -28,4 +28,22 @@ const general = new Router<CustomState, CustomContext>({
         ctx.body = progress
     })
 
+    .get("/course/:courseId/quiz-titles", accessControl(), async ctx => {
+        const courseId = ctx.params.courseId
+        const user = ctx.state.user
+        await checkAccessOrThrow(ctx.state.user, ctx.request.body.courseId, "view")
+
+        let quiz_titles: {[key: string]: string}  = {}
+
+        const result = await Quiz.getByCourseId(courseId)
+        for (const quiz of result) {
+            quiz_titles[quiz.id] = quiz.title
+            for (const quizItem of quiz.items) {
+                quiz_titles[quizItem.id] = quizItem.title
+            }
+        }
+
+        ctx.body = quiz_titles
+    })
+
 export default general
