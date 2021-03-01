8dd11101010e8dc6db1a281655fdc540a286b42e
d471061c <cpastethat@gmail.com>
2021-03-01 15:03:35 +0200

    setup tests for general api

---
+++ packages/backendv2/tests/generalApi.test.ts
@@ -0,0 +1,64 @@
+import request from "supertest"
+import nock from "nock"
+import app from "../app"
+import knex from "../database/knex"
+import { UserInfo } from "../src/types"
+
+import redis from "../config/redis"
+import { safeClean, safeSeed, configA } from "./util"
+
+afterAll(async () => {
+    await knex.destroy()
+    await redis.client?.flushall()
+})
+
+afterEach(async () => {
+    await redis.client?.flushall()
+})
+
+describe("general: course progress", () => {
+  beforeAll(async () => {
+    await safeSeed(configA)
+  })
+
+  afterAll(async () => {
+    nock.cleanAll()
+    await safeClean()
+  })
+
+  beforeEach(async () => {
+    nock("https://tmc.mooc.fi")
+      .get("/api/v8/users/current?show_user_fields=true")
+      .reply(function() {
+        const auth = this.req.headers.authorization
+        if (auth === "Bearer PLEB_TOKEN") {
+          return [
+            200,
+            {
+              id: 6666,
+              administrator: false,
+            } as UserInfo,
+          ]
+        }
+        if (auth === "Bearer ADMIN_TOKEN") {
+          return [
+            200,
+            {
+              administrator: true,
+            } as UserInfo,
+          ]
+        }
+      })
+  })
+
+  test("respond with 401 if invalid credentials", done => {
+    request(app.callback())
+      .get(
+        "/api/v2/general/course/46d7ceca-e1ed-508b-91b5-3cc8385fa44b/progress",
+      )
+      .set("Authorization", `bearer BAD_TOKEN`)
+      .expect(401)
+      .end(done)
+  })
+
+})
