5865446d8fa76b700e2aa9394998f9ac5e5d4332
heikki <heikki.pulli@helsinki.fi>
2021-04-09 09:39:35 +0300

    cleaner routes

--- packages/backendv2/src/controllers/kafka/index.ts
+++ packages/backendv2/src/controllers/kafka/index.ts
@@ -5,24 +5,15 @@ import { CustomState, CustomContext } from "../../types"
 
 const kafkaRoutes = new Router<CustomState, CustomContext>({
   prefix: "/kafka",
+}).get("/health/:topic?", accessControl(), async ctx => {
+  const topic = ctx.params.topic
+  const kafkaMessageCount = await KafkaMessage.getCurrentCountByTopic(topic)
+  console.log(kafkaMessageCount)
+  if (kafkaMessageCount <= 1000) {
+    ctx.status = 200
+  } else {
+    ctx.status = 500
+  }
 })
-  .get("/health", accessControl(), async ctx => {
-    const kafkaMessageCount = await KafkaMessage.getCurrentCountByTopic()
-    if (kafkaMessageCount < 1000) {
-      ctx.status = 200
-    } else {
-      ctx.status = 500
-    }
-  })
-  .get("/health/:topic", accessControl(), async ctx => {
-    const topic = ctx.params.topic
-    const kafkaMessageCount = await KafkaMessage.getCurrentCountByTopic(topic)
-    console.log(kafkaMessageCount)
-    if (kafkaMessageCount <= 1000) {
-      ctx.status = 200
-    } else {
-      ctx.status = 500
-    }
-  })
 
 export default kafkaRoutes
