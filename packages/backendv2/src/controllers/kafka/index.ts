3392bbcd39ae3bf9335c8ab2ae637b2dd9743acc
heikki <heikki.pulli@helsinki.fi>
2021-04-12 16:07:17 +0300

    useless auth check

--- packages/backendv2/src/controllers/kafka/index.ts
+++ packages/backendv2/src/controllers/kafka/index.ts
@@ -5,7 +5,7 @@ import { CustomState, CustomContext } from "../../types"
 
 const kafkaRoutes = new Router<CustomState, CustomContext>({
   prefix: "/kafka",
-}).get("/healthz/:topic?", accessControl(), async ctx => {
+}).get("/healthz/:topic?", async ctx => {
   const topic = ctx.params.topic
   const kafkaMessageCount = await KafkaMessage.getCurrentCountByTopic(topic)
   if (kafkaMessageCount <= 1000) {
