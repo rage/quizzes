6e615fe0dbeb04d9c284df8546d502fc98be06bf
Joel <joel.nhn@gmail.com>
2021-04-18 16:48:49 +0300

    add potential migration for moving checkbox stuff

---
+++ packages/backendv2/database/migrations/20210418130328_checkbox_quiz_item_migrations.ts
@@ -0,0 +1,40 @@
+import * as Knex from "knex"
+
+const moveQuizOptionAnswerRows = `
+    WITH moved_quiz_option_answer_rows AS (
+        DELETE FROM quiz_option_answer qoa
+            USING quiz_item_answer qia
+        WHERE qia.quiz_item_id IN (SELECT id FROM quiz_item WHERE type = 'checkbox');
+            # return old option id, and quiz_answer_id and quiz_item_if from item answer 
+        RETURNING qoa.id, qia.quiz_answer_id, qoa.quiz_option_id
+    )
+
+    INSERT INTO quiz_item_answer (id, quiz_answer_id, quiz_item_id)
+    SELECT * FROM moved_quiz_option_answer_rows;
+`
+
+const moveQuizOptionRows = `
+    WITH moved_quiz_option_rows AS (
+        DELETE FROM quiz_option qo
+            USING quiz_item qi
+        WHERE qi.type = 'checkbox'
+        RETURNING qo.id qo."order", qo.deleted, 'checkbox'::quiz_item_type_enum, qi.quiz_id  
+    )
+
+    INSERT INTO quiz_item (id, "order", deleted, type, quiz_id) 
+    SELECT * FROM moved_quiz_option_rows;
+`
+const deleteQuizOptionChildren = `
+    DELETE FROM quiz_option_translation qot
+            USING quiz_item qi, quiz_option qo
+            WHERE qi.type = 'checkbox'::quiz_item_type_enum 
+            AND qo.id IN (SELECT id FROM qi)
+`
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(moveQuizOptionAnswerRows)
+  await knex.raw(deleteQuizOptionChildren)
+  await knex.raw(moveQuizOptionRows)
+}
+
+export async function down(knex: Knex): Promise<void> {}
