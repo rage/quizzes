6b27a64737f24e1932afda6def40b59de103c040
d471061c <cpastethat@gmail.com>
2021-05-18 16:38:56 +0300

    add display policy migration

---
+++ packages/backendv2/database/migrations/20210518162600_quiz_display_policy.ts
@@ -0,0 +1,23 @@
+import * as Knex from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table("quiz_item", table => {
+    table
+      .enu(
+        "feedback_display_policy",
+        ["DisplayFeedbackOnQuizItem", "DisplayFeedbackOnAllOptions"],
+        {
+          useNative: true,
+          enumName: "quiz_item_feedback_display_policy",
+        },
+      )
+      .defaultTo("DisplayFeedbackOnQuizItem")
+      .notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("quiz_item", table => {
+    table.dropColumn("feedback_display_policy")
+  })
+}
