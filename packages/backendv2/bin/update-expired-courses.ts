0b50388442ccb2ce5aa26bd193c3c03f4e3e0f93
d471061c <cpastethat@gmail.com>
2021-05-26 07:02:57 +0300

    Indent sql-query

--- packages/backendv2/bin/update-expired-courses.ts
+++ packages/backendv2/bin/update-expired-courses.ts
@@ -5,12 +5,14 @@ const update_expired_courses = async (client: Knex) => {
   await client.raw(`
         update course as c
         set status = 'ended'
-        from (select result.course_id
-        from (select max(qa.created_at) as latest_course_submission, q.course_id as course_id
-        from quiz_answer qa
-        left join quiz q on qa.quiz_id = q.id
-        group by q.course_id) as result
-        where now() - result.latest_course_submission >= interval '1 year') as ended_courses
+        from (
+            select result.course_id
+              from (select max(qa.created_at) as latest_course_submission, q.course_id as course_id
+                    from quiz_answer qa
+                            left join quiz q on qa.quiz_id = q.id
+                    group by q.course_id) as result
+              where now() - result.latest_course_submission >= interval '1 year'
+            ) as ended_courses
         where ended_courses.course_id = c.id;
     `)
 }
