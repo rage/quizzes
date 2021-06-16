44c33e1d48d158bb9f7eff0c5a918cb7ecc80a14
Perkoila Kalle M <kalle.perkoila@helsinki.fi>
2021-06-16 13:06:56 +0300

    create table plagiarism task

--- packages/backendv2/database/migrations/20210616121834_quiz_add_column_check_plagiarism.ts
+++ packages/backendv2/database/migrations/20210616121834_quiz_add_column_check_plagiarism.ts
@@ -1,12 +1,11 @@
-import * as Knex from "knex";
-
+import * as Knex from "knex"
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.raw(`alter table quiz add column check_plagiarism boolean default false;`)
+  await knex.raw(
+    `alter table quiz add column check_plagiarism boolean default false;`,
+  )
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.raw(`alter table quiz drop column check_plagiarism;`)
+  await knex.raw(`alter table quiz drop column check_plagiarism;`)
 }
-
