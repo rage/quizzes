74cd513a31265b59372556202027c316b877410d
Henrik Nygren <nygrenh@gmail.com>
2021-09-20 21:29:17 +0300

    Add peer review id index to peer review question answer

---
+++ packages/backendv2/database/migrations/20210920212848_add_peer_review_id_index_to_peer_review_question_answer.ts
@@ -0,0 +1,14 @@
+import * as Knex from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table("peer_review_question_answer", table => {
+    table
+      .index("peer_review_id")
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("peer_review_question_answer", table => {
+    table.dropIndex("peer_review_id")
+  })
+}
