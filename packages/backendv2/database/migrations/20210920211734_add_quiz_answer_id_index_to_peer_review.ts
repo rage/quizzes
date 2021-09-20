ae146a0a393e2fee14e29ab90770230504b1dfe5
Henrik Nygren <nygrenh@gmail.com>
2021-09-20 21:20:19 +0300

    Add quiz_answer id index to peer review

---
+++ packages/backendv2/database/migrations/20210920211734_add_quiz_answer_id_index_to_peer_review.ts
@@ -0,0 +1,14 @@
+import * as Knex from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table("peer_review", table => {
+    table
+      .index("quiz_answer_id")
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("peer_review", table => {
+    table.dropIndex("quiz_answer_id")
+  })
+}
