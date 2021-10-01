03b76828e250cee380eca0c0d83240159c8a5b8f
Perkoila Kalle M <kalle.perkoila@helsinki.fi>
2021-10-01 18:45:33 +0300

    timeout csd request

--- packages/backendv2/src/services/plagiarism.ts
+++ packages/backendv2/src/services/plagiarism.ts
@@ -2,17 +2,25 @@ import axios from "axios"
 import jwt from "jsonwebtoken"
 import { GlobalLogger } from "../middleware/logger"
 
+const CancelToken = axios.CancelToken
+const source = CancelToken.source()
+
 export const relayNewAnswer = async (data: any) => {
   try {
-    await axios.post("http://localhost:5150/new", data, {
+    await axios.post("https://csd.mooc.fi/new", data, {
       headers: {
         authorization: jwt.sign(
           { source: "quizzes" },
           process.env.JWT_SECRET || "",
         ),
       },
+      cancelToken: source.token,
     })
+    setTimeout(() => {
+      GlobalLogger.warn("plagiarism detection: request timed out")
+      source.cancel()
+    }, 10000)
   } catch (error) {
-    GlobalLogger.error("plagiarism backend responded with error")
+    GlobalLogger.error("plagiarism detection: backend responded with error")
   }
 }
